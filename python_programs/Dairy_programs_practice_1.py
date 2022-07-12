125fde1c142f5fc386e80368a51c1e263cccd5dd
firoz <firozbabu1a0555@gmail.com>
2022-07-12 17:53:18 +0530

    left and right diagonal elements find in matrix

--- python_programs/Dairy_programs_practice_1.py
+++ python_programs/Dairy_programs_practice_1.py
@@ -6,7 +6,7 @@ to find the spy number
 
 """
 
-
+'''
 p = int(input())
 sum = 0
 prod = 1
@@ -18,3 +18,23 @@ while p:
 
 
 print(sum==prod)
+
+'''
+
+
+l  = [[1,2,3,4],[4,3,2,1],[7,8,9,6],[6,5,4,3]]
+
+k  = 0
+m = len(l)
+
+
+left_diag = []
+right_diag=[]
+for i in range(len(l)):
+    left_diag.append(l[i][k])
+    k+=1
+    right_diag.append(l[i][m-1])
+    m-=1
+
+print(left_diag)
+print(right_diag)
